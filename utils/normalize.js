a8cfdeec27e8d6cb47c1ca6b4b306de2ad6c74dc
Yogesh Pathe <98703537+yugyogeshpawar@users.noreply.github.com>
2023-11-03 13:46:34 +0530

    changes

--- utils/normalize.js
+++ utils/normalize.js
@@ -1,20 +1,16 @@
-
+export const normalizeOwnedCourse = (web3) => (course, ownedCourse) => {
+  return {
+    ...course,
+    ownedCourseID: ownedCourse.id,
+    proof: ownedCourse.proof,
+    owned: ownedCourse.owner,
+    price: web3.utils.fromWei(ownedCourse.price),
+    state: COURSE_STATE[ownedCourse.state],
+  };
+};
 
 export const COURSE_STATE = {
-    0 : "purchased",
-    1 : "activated",
-    2 : "deactivated"
-}
-
-
-
-export const normalizeOwnedCourse = web3 => (course, ownedCourse) => {
-    return {
-        ...course,
-        ownedCourseID: ownedCourse.id,
-        proof: ownedCourse.proof,
-        owned: ownedCourse.owner,
-        price: web3.utils.fromWei(ownedCourse.price),
-        state: COURSE_STATE[ownedCourse.state]
-    }
-}
+  0: "purchased",
+  1: "activated",
+  2: "deactivated",
+};
